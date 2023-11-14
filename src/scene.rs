0d2c1ce85fd1cfe9eef2d607d634851438e5f184
Aleksandr <61507854+FairlyBet@users.noreply.github.com>
2023-11-15 01:33:46 +0200

    several updates of scene system

--- src/scene.rs
+++ src/scene.rs
@@ -1,23 +1,39 @@
 use crate::{camera::Camera, data3d::Model, lighting::LightSource, linear::Transform};
+use serde::Deserialize;
+use std::fs;
 
-pub struct Scene {
-    transform_buffer: Vec<Transform>
-}
+const ENTITIES_FILENAME: &str = "entities.json";
+const TRANSFORMS_FILENAME: &str = "transforms.json";
+
+pub struct Scene {}
 
 impl Scene {
-    pub fn load() {
+    pub fn load_from_file(path: &str) {
+        let scene_dir = std::path::Path::new(path);
+
+        let path = scene_dir.with_file_name(ENTITIES_FILENAME);
+
+        let path = scene_dir.with_file_name(TRANSFORMS_FILENAME);
+        let transforms_str = fs::read_to_string(path).unwrap();
         // load from file
         // create entities
         // create components
     }
-}
 
+    fn read_vec<'a, T>(path: &str) -> Vec<T>
+    where
+        T: Deserialize<'a>,
+    {
+        // let values_str = ;
+        let result = serde_json::from_::<Vec<T>>(&fs::read_to_string(path).unwrap()).unwrap();
+        result
+    }
+} 
+
+#[derive(Deserialize)]
 struct Entity {
     id: u32,
     name: String,
-    transform: *const Transform,
 }
 
-struct Component {
-
-}
+struct Component {}
