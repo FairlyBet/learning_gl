19f41a0ad84e94993209fbe783e718ed4b21b9ff
Aleksandr <61507854+FairlyBet@users.noreply.github.com>
2024-03-06 21:59:52 +0200

    .

--- assets/scripts/script.lua
+++ assets/scripts/script.lua
@@ -1,5 +1,17 @@
-local object = {}
+ObjectPool = {}
 
-function object:update()
+function ObjectPool:__newindex(k, v)
+
+end
+
+function ObjectPool:update()
     self.gameObject.transform.move(Vec3.zeros())
 end
+
+function ObjectPool.new()
+    local object = {}
+    setmetatable(object, ObjectPool)
+    return object
+end
+
+return ObjectPool.new
